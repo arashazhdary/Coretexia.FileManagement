2a0b5414f84e9fcd497a16ab3c97aaa7e65f4bc9
Arash Ajdari <Arash.azhdary@gmail.com>
2024-11-20 16:51:48 +0330

    .

---
+++ MinIO/Cortexia.FileManagement.MinIO/Services/MinIOFileManagement.cs
@@ -0,0 +1,28 @@
+﻿using Coretexia.FileManagement.Abstraction.Models.DeleteServices;
+using Coretexia.FileManagement.Abstraction.Models.DownloadServices;
+using Coretexia.FileManagement.Abstraction.Models.UploadServices;
+using Coretexia.FileManagement.Abstraction.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cortexia.FileManagement.MinIO.Services;
+public class MinIOFileManagement : IFileManagement
+{
+    public Task<FileManagementDeleteServiceResult?> Delete(FileManagementDeleteServiceInput input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<FileManagementDownloadServiceResult?> Download(FileManagementDownloadServiceInput input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<FileManagementUploadServiceResult?> Upload(FileManagementUploadServiceInput input)
+    {
+        throw new NotImplementedException();
+    }
+}
